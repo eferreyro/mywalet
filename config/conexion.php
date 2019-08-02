d209c25ea5f73811d98de95fc1253aed75305593
eferreyro <emafe7472@gmail.com>
2019-08-02 13:29:28 -0500

    Agregando AdminLTE al proyecto

--- config/conexion.php
+++ config/conexion.php
@@ -1,23 +1,44 @@
 <?php
-class Conectar {
-	protected $dbh;
 
-	protected function conexion(){
-	try {
-		$conectar = $this->dbh = new PDO("
-			mysql:local=localhost;dbname=dbproyecto","root","");
-				return $conectar;
-			} catch (Exception $e){
-				print "!Error... " . $e->getMessage() . "<br/>";
-				die();
-			}
-		} //Cierre llave Funcion conexion
-		public function set_names(){
-			return $this->dbh->query("SET NAMES 'utf8'");
-			}
-			public function ruta(){
-				return "http://localhost/proyecto";
-			}
-	} //Cierre llave conectar
 
+ class Conectar {
+
+ 	protected $dbh;
+
+ 	protected function conexion(){
+
+
+ 		try {
+
+ 			$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=dbproyecto","root","");
+		   
+		     return $conectar;
+ 			
+ 		} catch (Exception $e) {
+
+ 			print "¡Error!: " . $e->getMessage() . "<br/>";
+            die();  
+ 			
+ 		}
+ 
+
+
+		 } //cierre de llave de la function conexion()
+
+
+		 public function set_names(){
+
+		 	return $this->dbh->query("SET NAMES 'utf8'");
+		 }
+
+
+		 public function ruta(){
+
+		 	return "http://localhost/proyecto/";
+		 }
+
+	
+
+	}//cierre de llave conectar 		
+		  	
 ?>
